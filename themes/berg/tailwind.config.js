d5b5246cf8ce6a052fd18723e8d852e08abc32f5
Matus Stafura <kobe21@gmail.com>
2023-05-13 18:32:26 +0200

    theme update

--- themes/berg/tailwind.config.js
+++ themes/berg/tailwind.config.js
@@ -1,11 +1,44 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
     content: [
         "./layouts/**/*.html",
+        "./content/**/*.html",
+        "./themes/berg/layouts/**/*.html",
+        "./themes/berg/content/**/*.{html,md}",
     ],
     darkMode: 'class', 
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+               primary: {
+                50: "var(--color-primary-50)",
+                100: "var(--color-primary-100)",
+                200: "var(--color-primary-200)",
+                300: "var(--color-primary-300)",
+                400: "var(--color-primary-400)",
+                500: "var(--color-primary-500)",
+                600: "var(--color-primary-600)",
+                700: "var(--color-primary-700)",
+                800: "var(--color-primary-800)",
+                900: "var(--color-primary-900)",
+                950: "var(--color-primary-950)",
+                },
+               secondary: {
+                50: "var(--color-secondary-50)",
+                100: "var(--color-secondary-100)",
+                200: "var(--color-secondary-200)",
+                300: "var(--color-secondary-300)",
+                400: "var(--color-secondary-400)",
+                500: "var(--color-secondary-500)",
+                600: "var(--color-secondary-600)",
+                700: "var(--color-secondary-700)",
+                800: "var(--color-secondary-800)",
+                900: "var(--color-secondary-900)",
+                950: "var(--color-secondary-950)",
+                },
+            },
+      },
     },
     plugins: [
         require('@tailwindcss/typography'),
